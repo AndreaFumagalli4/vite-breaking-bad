f0aecba675ae9e9702339110bb08f010e7352994
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-01-11 14:34:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default{
+  components: {
+    AppHeader,
+
+  }
+}
+</script>
+
+<template>
+
+  <header>
+    <AppHeader />
+  </header>
+
+</template>
+
+<style>
+
+</style>
